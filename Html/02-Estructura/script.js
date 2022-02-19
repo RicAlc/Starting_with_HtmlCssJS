d34b8ff5bd787d9d1addd55dcf766bff9189a48c
RicAlc <94550398+RicAlc@users.noreply.github.com>
2022-02-18 22:58:26 -0600

    add: html basics

---
+++ Html/02-Estructura/script.js
@@ -0,0 +1,8 @@
+document.querySelector('h1').addEventListener('click', () => {
+alert('alto ahi, no hagas click en el titulo');
+document.body.style.background = 'red';
+document.body.style.background = 'black';
+
+
+location.reload();
+} )
